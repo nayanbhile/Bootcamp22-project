df13f7838b1dd4614294d1d2e97ab9acade49c20
Nayan Bhile <nayan.bhile@tothenew.com>
2022-03-29 12:17:08 +0530

    Initial commit- Project initialized

---
+++ src/main/java/com/ttn/Bootcamp22project/Bootcamp22ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ttn.Bootcamp22project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootcamp22ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootcamp22ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttn/Bootcamp22project/Bootcamp22ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttn.Bootcamp22project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bootcamp22ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
